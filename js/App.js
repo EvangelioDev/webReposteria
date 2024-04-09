54e3a3c87fb306fec5b938f65039f1477d1bb51d
EvangelioDev <clansevangelio@gmail.com>
2024-04-09 01:00:31 -0400

    Apply style and format in nav-bar and add slide with video

--- js/App.js
+++ js/App.js
@@ -15,6 +15,33 @@ const visibleMenu = () => {
 }
 
 const visibleBtn = () => {
-    console.log('visibilidad del boton');
+    const btnClose = document.createElement('p');
+    btnClose.textContent = 'x';
+    btnClose.classList.add('btn-close');
+    nav_menu.appendChild(btnClose); 
+    
+    /* difurminar */
+    const body = document.querySelector('body');
+    const overlay = document.createElement('div');
+    if(document.querySelectorAll('.back').length > 0) return;
+    overlay.classList.add('back');
+    body.appendChild(overlay);
+
+    handlerClose(btnClose, overlay);
+}
+
+const handlerClose = (button, overlay) =>
+{
+    button.addEventListener('click', () => {
+        nav_menu.classList.add('hidden');
+        overlay.remove();
+        button.remove();
+    });
+
+    overlay.addEventListener('click', () => {
+        nav_menu.classList.add('hidden');
+        overlay.remove();
+        button.remove();
+    });
 }
 
